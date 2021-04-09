204da13e96b49b08d064280d4bd6e82ab3ac4523
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-04-08 20:13:30 -0500

    <GamePieces>[Objects]: Setup additional packages
    [No Issue]

--- GamePiece.py
+++ GamePiece.py
@@ -3,6 +3,10 @@ from typing import cast
 
 from arcade import Sprite
 
+from pytrek.Constants import STANDARD_SPRITE_HEIGHT
+from pytrek.Constants import STANDARD_SPRITE_WIDTH
+from pytrek.Constants import STANDARD_X_ADJUSTMENT
+from pytrek.Constants import STANDARD_Y_ADJUSTMENT
 from pytrek.objects.Coordinates import Coordinates
 
 
@@ -20,3 +24,12 @@ class GamePiece(Sprite):
     @currentPosition.setter
     def currentPosition(self, newValue: Coordinates):
         self._currentPosition = newValue
+
+    def gamePositionToScreenPosition(self, gameCoordinates: Coordinates):
+
+        sectorX: int = gameCoordinates.x
+        sectorY: int = gameCoordinates.y
+        x = ((sectorX * STANDARD_SPRITE_WIDTH)  * 2) + STANDARD_X_ADJUSTMENT
+        y = ((sectorY * STANDARD_SPRITE_HEIGHT) * 2) + STANDARD_Y_ADJUSTMENT
+
+        return x, y
