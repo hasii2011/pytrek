12bd1a5460e9a58a80cadbb850710d453dfb1427
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-05-12 13:06:10 -0500

    <Feature>[Klingon Torpedo Followers]: Follower's working
    They delete on reaching target; However, not deleted if missed
    
    [No Issue]

---
+++ pytrek/gui/gamepieces/KlingonTorpedoFollower.py
@@ -0,0 +1,34 @@
+
+from typing import cast
+
+from uuid import uuid4
+
+from pytrek.LocateResources import LocateResources
+from pytrek.gui.gamepieces.GamePiece import GamePiece
+
+
+class KlingonTorpedoFollower(GamePiece):
+
+    def __init__(self):
+
+        fqFileName: str = LocateResources.getResourcesPath(resourcePackageName=LocateResources.IMAGE_RESOURCES_PACKAGE_NAME,
+                                                           bareFileName='KlingonTorpedoFollower-16.png')
+
+        GamePiece.__init__(self, filename=fqFileName)
+
+        self._following: uuid4 = cast(uuid4, None)
+
+    @property
+    def following(self) -> uuid4:
+        """
+        """
+        return self._following
+
+    @following.setter
+    def following(self, newValue: uuid4):
+        """
+        Args:
+            newValue:
+        """
+        self._following = newValue
+
