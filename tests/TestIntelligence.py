c748733506b4b03f23e1bf74d647308eadac1c1a
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-06-01 15:13:23 -0500

    <Feature>[Unit Test]: Adjust Range
    Do this anecdotally
    
    [No Issue]

--- tests/TestIntelligence.py
+++ tests/TestIntelligence.py
@@ -314,7 +314,7 @@ class TestIntelligence(TestBase):
 
         medianStatistic: float = self._runCommanderPowerTest()
 
-        ans: bool = (medianStatistic > 1380.0) and (medianStatistic < 1425.0)
+        ans: bool = (medianStatistic > 1360.0) and (medianStatistic < 1425.0)
 
         self.assertTrue(ans, f'We are not in range: {medianStatistic=}')
 
