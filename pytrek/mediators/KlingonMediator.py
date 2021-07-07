4430e2a8d8c9847cc428a2f2a176e4ef88f4b77f
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-07-07 13:30:10 -0500

    <Enhancement>[Klingon]: Use common logic
    https://github.com/hasii2011/PyArcadeStarTrek/issues/24

--- pytrek/mediators/KlingonMediator.py
+++ pytrek/mediators/KlingonMediator.py
@@ -6,14 +6,7 @@ from logging import getLogger
 
 from arcade import Sound
 
-from pytrek.engine.ArcadePoint import ArcadePoint
-from pytrek.engine.PlayerType import PlayerType
-
-from pytrek.gui.gamepieces.GamePiece import GamePiece
-
 from pytrek.gui.gamepieces.klingon.Klingon import Klingon
-
-from pytrek.model.Coordinates import Coordinates
 
 from pytrek.model.Quadrant import Quadrant
 
@@ -32,31 +25,13 @@ class KlingonMediator(BaseEnemyMediator):
         self._loadSounds()
 
     def update(self, quadrant: Quadrant, klingon: Klingon):
+        self.moveEnemy(quadrant=quadrant, enemy=klingon)
 
-        playerType: PlayerType = self._gameState.playerType
-        if playerType == PlayerType.Emeritus or playerType == PlayerType.Expert:
-
-            currentTime:    float = self._gameEngine.gameClock
-            deltaClockTime: float = currentTime - klingon.timeSinceMovement
-            if deltaClockTime > klingon.moveInterval:
-
-                oldPosition: Coordinates = klingon.gameCoordinates
-                newPosition: Coordinates = self._evade(currentLocation=oldPosition)
-                self._enemyMovedUpdateQuadrant(quadrant=quadrant, enemy=klingon, newSectorCoordinates=newPosition, oldSectorCoordinates=oldPosition)
-
-                klingon.gameCoordinates = newPosition
-                self._toArcadePoint(klingon, newPosition)
-                klingon.timeSinceMovement = currentTime
-
-                self._klingonMove.play(self._gameSettings.soundVolume.value)
-
-        else:
-            arcadePoint: ArcadePoint = GamePiece.gamePositionToScreenPosition(klingon.gameCoordinates)
-
-            self.logger.debug(f'{arcadePoint=}')
-            # assert arcadePoint.y == 0.0, 'This sprite is off quadrant'
-            klingon.center_x = arcadePoint.x
-            klingon.center_y = arcadePoint.y
+    def _playMoveSound(self):
+        """
+        Override super class
+        """
+        self._klingonMove.play(self._gameSettings.soundVolume.value)
 
     def _loadSounds(self):
 
