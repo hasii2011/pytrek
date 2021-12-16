fe6cd9587a0dcf3d3c73c687b9c3aa3345f60220
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-12-16 14:42:31 -0700

    <Unit Test>[Intelligence]: Older methods ranging in
    Fix ranges
    
    https://github.com/hasii2011/PyArcadeStarTrek/issues/78

--- tests/pytrek/engine/TestIntelligence.py
+++ tests/pytrek/engine/TestIntelligence.py
@@ -413,7 +413,7 @@ class TestIntelligence(TestBase):
 
         for x in range(TestIntelligence.RANGE_TESTS_LOOP_COUNT):
             medianStatistic: float = self._runPowerTest(computeCallback=self.smarty.computeCommanderPower)
-            ans:             bool = (medianStatistic > 1348.0) and (medianStatistic < 1451.0)
+            ans:             bool = (medianStatistic > 1348.0) and (medianStatistic < 1452.0)
             self.assertTrue(ans, f'We are not in range: {medianStatistic=}')
 
     def testComputeSuperCommanderPowerExpertPlayer(self):
